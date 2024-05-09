0f330d3c72ca7b5ff67c511ce8214278a4072b32
Varshenee <varshenee01@gmail.com>
2024-05-09 10:52:00 +0530

    Index

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+function filterMovies() {
+    let input = document.getElementById('search-input').value.toUpperCase();
+    let moviesContainer = document.getElementById('movies');
+    let movies = moviesContainer.getElementsByClassName('movie');
+    let searchText, movieTitle;
+
+    for (let i = 0; i < movies.length; i++) {
+        movieTitle = movies[i].getElementsByClassName("movie-title")[0];
+        searchText = movieTitle.textContent || movieTitle.innerText;
+        if (searchText.toUpperCase().indexOf(input) > -1) {
+            movies[i].style.display = "";
+        } else {
+            movies[i].style.display = "none";
+        }
+    }
+}
