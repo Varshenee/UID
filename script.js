c617df8f078a0f18021723a72e0dda76a7dcf329
AnaswaraSyam <143377901+AnaswaraSyam@users.noreply.github.com>
2024-05-21 19:52:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,11 @@ function filterMovies() {
         }
     }
 }
+
+function selectMovie(movieId) {
+    window.location.href = 'movie-details.html?movieId=' + movieId;
+}
+
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
